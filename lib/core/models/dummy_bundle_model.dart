4a0faf528e08965a86ed79b18051f0aa5c8f83d3
benz317 <145668786+benz317@users.noreply.github.com>
2025-01-21 15:09:29 +0700

    first commit

---
+++ lib/core/models/dummy_bundle_model.dart
@@ -0,0 +1,15 @@
+class BundleModelItem {
+  String name;
+  String cover;
+  List<String> itemNames;
+  double price;
+  double mainPrice;
+
+  BundleModelItem({
+    required this.name,
+    required this.cover,
+    required this.itemNames,
+    required this.price,
+    required this.mainPrice,
+  });
+}
